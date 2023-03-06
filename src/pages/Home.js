29095ad9a8a7cebaf243ac6bb2bf4a11aa9360dc
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-03-06 12:10:55 +0100

    gestion readData et ErrorMsg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,79 +6,85 @@ import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined
 //! installer dans cet ordre : 
 //! yarn add @mui/material @emotion/react @emotion/styled
 
-
 // export class Credential { constructor(name, password) {  
 //         this.name = name;
 //         this.password = password; }}
 
+//* Dataline contient id (= user + date) + credential qui contient name + password
 // export class DataLine {constructor(id, credential) {
 //         this.id = id;
 //         this.credential = credential;}}
 
-//* Dataline contient id (= user + date) ET credential qui contient name + password
 
 const Home = () => {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
-    const [dataline, setDataline] = useState([]);
     const [data, setData] = useState([]);    // if (data == null) data = [];
-    // id: { id }, credential: { name: { name }, password: { password } } 
+    const [read, setRead] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
 
-    // let data = JSON.parse(localStorage.getItem('data'));
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const id = 'user' + Date.now(); // const timestamp = Date.now();
-        console.log('id : ', id); //ok
 
-        const credential = { name: name, password: password }; // ok
+        // pertinent que credential soit un objet (= une ligne d'infos sur l'utilisateur)
+        const credential = { id: id, name: name, password: password };
         console.log('credential : ', credential);
-        setDataline({ id: id, credential: credential })   // const line = new DataLine(id, cred);// data = [...data, line];
-        console.log('dataline : ', dataline);
 
-        setData(dataline);
+        //? pertinent que le state data soit un tableau, qui contiendra plusieurs objet credential
+        //! on change la valeur du state pour pouvoir l'afficher et l'utiliser ailleurs
+        const copy = [...data];  // data = [...data, line];
+        copy.push(credential); // on push sur la copie du tableau data pour ajouter une entrée
+        console.log("copy: ", copy);
+        setData(copy)   // const line = new DataLine(id, cred);
 
-        // setData((prevState) => [...prevState, {id: id, credential: credential,},]);
-        // push ne fonctionne pas
+        /* stocker exactement ce qu'on stocke ds data/ = ici ce qu'on stocke ds le state, 
+        car le temps que le setData se mette à jour le storage sera déjà fait, l'un ne devrait pas dépendre de l'autre*/
+        localStorage.setItem('data', JSON.stringify(copy));
 
-        console.log('data : ', data); //* attention les logs ont 1 temps de retard
+        // console.log('data : ', data); //* attention les logs ont 1 temps de retard
         setName("");
         setPassword("");
+        setErrorMsg(""); // Je fais disparaitre le message d'erreur et les input du form
     };
 
-    //saving data to local storage
+    // Récupérer les données dans le but de les afficher par exemple
     useEffect(() => {
-        localStorage.setItem('data', JSON.stringify(data));
-        localStorage.setItem('dataline', JSON.stringify(dataline));
-    }, [data]); // pour enregistrer les données à chaque fois qu'elles sont modifiées
-
-    // lire les données
-    useEffect(() => {
-        const items = JSON.parse(localStorage.getItem('data'));
-        if (items) {
-            setData(items);
+        const items = localStorage.getItem('data');  // let data = JSON.parse(localStorage.getItem('data'));
+        if (items) {  // vérifier qu'il existe des datas avat de parser sinon erreur
+            setData(JSON.parse(items));
         }
-    }, []); //tableau vide car nous ne voulons obtenir les données qu'une seule fois au chargement de la page
+    }, []); //* tableau vide car nous ne voulons obtenir les données qu'une seule fois au chargement de la page
 
-
-    //! efface la dernière clé ds le storage
-    const remove = () => {
-        localStorage.removeItem('data'); // ne fonctionne plus si les données s'écrasent
-    };
+    // //! efface la dernière clé ds le storage
+    // const remove = () => {
+    //     localStorage.removeItem('data');
+    // };
 
     //! efface toutes les clés ds le storage
     const removeAllData = () => {
-        localStorage.clear('data'); // ok
+        localStorage.clear(); // ok
+        setData("");
+        setRead(false); //! ne pas oublier de remettre read sur false
     };
 
     // lire  la clé ds le storage   //! ne fonctionne pas
     const readData = () => {
-        localStorage.getItem('data') && (
-            <div>
-                <p>Name: {JSON.parse(localStorage.getItem('data'))}</p>
-            </div>
-        )
+        if (data) {
+            localStorage.getItem('data') && (
+                // attention vérifier qu'il existe des datas avat de parser sinon erreur
+                JSON.parse(localStorage.getItem('data'))
+            )
+            setRead(true);  // condition pour afficher les données
+        } else {
+            setErrorMsg("il n'y a rien à afficher, veuillez entrer une donnée");
+            console.log(errorMsg);
+        }
     }
+
+
+
 
     return (<>
         <div className="App">
@@ -101,31 +107,36 @@ const Home = () => {
                             onChange={(e) => setPassword(e.target.value)}
                         /></label>
                     <div className='button'>
+
+                        {/* //! par défaut un button est de type submit */}
                         <button type="submit">Done</button>
-                        <button onClick={remove}>Remove last data
-                        </button>
-                        <button className="btn_delete" disabled={true} >Remove ALL data
+
+                        {/* <button type="button" onClick={remove}>Remove last data </button>*/}
+                        {/* //! si on ne veut pas qu'il redéclenche la fonction liée au onSubmit, il faut les passer en type=button */}
+                        <button type="button" className="btn_delete" disabled={true} >Remove ALL data
                             <DeleteForeverOutlinedIcon
                                 className="note_delete"
                                 aria-hidden="true"
                                 onClick={removeAllData}
                             ></DeleteForeverOutlinedIcon>
                         </button>
-                        <button onClick={readData}>Read data</button>
 
+                        <button type="button" onClick={readData}>Read data</button>
+                        {/* data.length > 0 */}
+             {read ? (
+                        data.map((elem) => {
+                            // console.log(elem);
+                            return <div key={elem.id}>
+                                <span>Bonjour {`${elem.name.charAt(0).toUpperCase() + elem.name.slice(1)}`}</span>
+                            </div>
+                        })
+                    ) : (
+                            <p className={errorMsg && "error_msg"}>{errorMsg}</p>
+                        )}
                     </div>
-
-                    {/* {localStorage.getItem('Password') && (
-                        <div>
-                            <p>Password: {localStorage.getItem('Password')}</p>
-                        </div>
-                    )} */}
                 </form>
-
             </main>
-
         </div>
-
     </>);
 };
 export default Home;
